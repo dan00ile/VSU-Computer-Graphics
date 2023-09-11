f65e5ae047df58a861c99280cbf15f00095aace5
Даниил <125554382+dan00ile@users.noreply.github.com>
2023-09-11 22:04:06 +0300

    Created DrawPanel for output of graphic info

---
+++ src/ru/vsu/cs/shul/task1/DrawPanel.java
@@ -0,0 +1,15 @@
+package ru.vsu.cs.shul.task1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+
+    public DrawPanel() {
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
--- src/ru/vsu/cs/shul/task1/MainWindow.java
+++ src/ru/vsu/cs/shul/task1/MainWindow.java
@@ -1,7 +1,14 @@
 package ru.vsu.cs.shul.task1;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
+
+    public MainWindow() throws HeadlessException {
+        DrawPanel dp = new DrawPanel();
+        this.add(dp);
+    }
+
 
 }
